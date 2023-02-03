5e2b6808c58d14de59c9b7cff9e6de2af1fc6e1e
abbos <abbos@gmail.com>
2023-02-03 12:12:12 +0300

    confugiration

---
+++ src/context/properties/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useReducer } from 'react'
+import { createContext } from 'react'
+import { reducer } from './reducer'
+
+const PropertiesContext=createContext()
+
+export const PropertiesProvider = ({children}) => {
+  const [state,dispatch]=useReducer(reducer,[])
+  return (
+    <PropertiesContext.Provider>
+      {children}
+    </PropertiesContext.Provider>
+  )
+}
